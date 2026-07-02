888f044f4f7744a505bd3b6137371fb77c0ecd6a
Joseph Nguyen <joseph.b.nguyen@gmail.com>
2026-07-02 16:36:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # This is the HTML text that will be displayed on your website
-    return "<h1>JBNX.IO // Hello World!</h1>"
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    # Railway assigns a dynamic port, so we must fetch it from the environment
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
